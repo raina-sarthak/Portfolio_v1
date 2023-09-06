28ff94d209e94962e4f5c3a19b08bbe7098f02e9
raina-sarthak <sarthakraina@MacBook.local>
2023-09-06 18:09:05 +0530

    Propro

---
+++ src/app/components/errorpage.js
@@ -0,0 +1,9 @@
+export default function Error(){
+    return(
+        <div class="grid h-screen px-4 bg-white place-content-center dark:bg-gray-900">
+            <h1 class="tracking-widest text-gray-500 uppercase dark:text-gray-400">
+                404 | Not Found
+            </h1>
+        </div>
+    )
+}
---
+++ src/app/components/heropage.js
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return(
+        <main className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+            <div>HERO</div>
+        </main>
+    )
+}
